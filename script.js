ef4b6b97f28ab5aa5a368b7869337a5b4b35c3a3
Rianone <84690507+Rianone@users.noreply.github.com>
2023-03-25 22:01:56 +0100

    updated script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const word = document.querySelector('.generted-word');
 
 button.addEventListener("click", () => {
 
-    fetch("https://api.api-ninjas.com/v1/randomword").then((res) => {
-        console.log(res.data)
+    fetch("https://random-word-api.herokuapp.com/word").then((res) => {
+        console.log(res)
     })
 
-    word.innerHTML = "Hello"
-    word.style.animation = "spin 1s alternate ease-in-out";
-    word.style.transition = "animation 1s ease-in";
-    word.style.animation = "grow 1s alternate ease-in-out";
+    // word.innerHTML = "Hello"
+    // word.style.animation = "spin 1s alternate ease-in-out";
+    // word.style.transition = "animation 1s ease-in";
+    // word.style.animation = "grow 1s alternate ease-in-out";
 
 });
