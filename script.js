1d4ce3f83251a0a91df5fee6286c18bb041d4af0
Rianone <84690507+Rianone@users.noreply.github.com>
2023-04-02 22:24:27 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const button = document.querySelector(".btn");
 const word = document.querySelector('.generted-word');
+const animation_array = ["spin", "grow", "changeColor1", "changeColor2", "changeColor3", "changeColor4"]
+
+function random(number) {
+    return Math.floor(Math.random() * number)
+}
 
 button.addEventListener("click", () => {
+    fetch("./data.json").then((res) => res.json()).then((res) => {
+        let word1 = res[random(178190)];
+        let word2 = res[random(178190)];
 
-    fetch("https://random-word-api.herokuapp.com/word").then((res) => {
-        console.log(res)
+        word.style.animation = animation_array[random(6)] + " 1s alternate ease-in-out";
+        word.style.transition = "animation 1s ease-in";
+        word.innerHTML = word1 + " " + word2;
+
+    }).catch((err) => {
+        console.log(err)
     })
 
-    // word.innerHTML = "Hello"
-    // word.style.animation = "spin 1s alternate ease-in-out";
-    // word.style.transition = "animation 1s ease-in";
-    // word.style.animation = "grow 1s alternate ease-in-out";
 
 });
